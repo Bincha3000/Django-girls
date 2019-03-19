13b8b18d8caf5e06fafc772bb1ebec528b59a5de
Bincha3000 <bincha.1997@gmail.ru>
2019-03-19 13:31:20 +1000

    Added registration page.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render, get_object_or_404, \
                              redirect
+from django.contrib.auth import authenticate, login
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic import TemplateView
+
+
+class LoginView(TemplateView):
+    template_name = "registration/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        context = {}
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                context['error'] = "Неправильно введён логин или пароль"
+        return render(request, self.template_name, context)
+
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
