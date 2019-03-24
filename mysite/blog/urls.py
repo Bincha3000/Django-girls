6cee48867dc1bc06bc672417cb192f12cb883d42
Bincha3000 <bincha.1997@gmail.ru>
2019-03-24 21:34:03 +1000

    Add profile page

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 from . import views
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, ProfilePage
 
 urlpatterns = [
-    re_path(r'^accounts/register/$', RegisterView.as_view(), name="register"),
-    re_path(r'^account/login/$', LoginView.as_view(), name='login'),
+    re_path(r'^accounts/register/$', RegisterView.as_view(), name='register'),
+    re_path(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    re_path(r'^accounts/profile/$', ProfilePage.as_view(), name='profile'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -43,6 +43,9 @@ class LoginView(TemplateView):
         return render(request, self.template_name, context)
 
 
+class ProfilePage(TemplateView):
+    template_name = "registration/profile.html"
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
